0e7557e52125b827fde681ba6a72be35711db36c
gsulc <gordon.sulc@gmail.com>
2018-10-20 13:27:43 -0400

    Grid view of objects
    grid view and remove selected item

--- src/ModbusInteractionModule/ViewModels/ModbusInteractionViewModel.cs
+++ src/ModbusInteractionModule/ViewModels/ModbusInteractionViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using NModbus.UI.Common.Core;
 using NModbus.UI.Service;
+using Prism.Commands;
 using Prism.Events;
 using Prism.Mvvm;
 using Prism.Regions;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -22,9 +24,7 @@ namespace NModbus.UI.InteractionModule.ViewModels
         {
             _eventAggregator = ea;
             ea.GetEvent<NewModbusMasterEvent>().Subscribe(NewModbusMaster);
-            //_lineItems.Add(new LineItem());
-            //LineItems = _lineItems;
-            LineItems.Add(new LineItem());
+            RemoveSelectedCommand = new DelegateCommand<IList>(RemoveSelectedItems);
         }
 
         public ObservableCollection<LineItem> LineItems
@@ -33,11 +33,26 @@ namespace NModbus.UI.InteractionModule.ViewModels
             set => SetProperty(ref _lineItems, value);
         }
 
+        private LineItem _selectedItem;
+        public LineItem SelectedItem
+        {
+            get => _selectedItem;
+            set => SetProperty(ref _selectedItem, value);
+        }
+
+        public DelegateCommand<IList> RemoveSelectedCommand { get; private set; }
+
+        private void RemoveSelectedItems(IList items)
+        {
+            object[] arr = new object[items.Count];
+            items.CopyTo(arr, 0);
+            foreach (var item in arr)
+                LineItems.Remove(item as LineItem);
+        }
+
         private void NewModbusMaster(IModbusMaster master)
         {
             _master = master;
         }
-
-        
     }
 }
