15a7ed32a7e9f26bae825bc77337dc073723eff0
gsulc <gordon.sulc@gmail.com>
2019-05-23 01:04:48 -0400

    cleanup modbus master manager
    heavy cleanup

---
+++ src/Service/FactoryExtensions.cs
@@ -0,0 +1,63 @@
+﻿using NModbus.IO;
+using NModbus.Serial;
+using NModbus.UI.Common.Core;
+using System;
+using System.IO.Ports;
+using System.Net.Sockets;
+
+namespace NModbus.UI.Service
+{
+    public static class FactoryExtensions
+    {
+        public static IModbusMaster CreateIpMaster(this IModbusFactory factory, IpSettings ipSettings)
+        {
+            switch (ipSettings.ModbusType)
+            {
+                case ModbusType.Tcp:
+                    return factory.CreateMaster(
+                        new TcpClient(ipSettings.Hostname, ipSettings.Port));
+                case ModbusType.Udp:
+                    return factory.CreateMaster(
+                        new UdpClient(ipSettings.Hostname, ipSettings.Port));
+                case ModbusType.RtuOverTcp:
+                    return factory.CreateRtuMaster(
+                        new TcpClientAdapter(
+                            new TcpClient(ipSettings.Hostname, ipSettings.Port)));
+                case ModbusType.RtuOverUdp:
+                    return factory.CreateRtuMaster(
+                        new UdpClientAdapter(
+                            new UdpClient(ipSettings.Hostname, ipSettings.Port)));
+                default:
+                    throw new ArgumentException(
+                        "Ip settings must be of type Tcp, Udp, RtuOverTcp, or RtuOverUdp.");
+            }
+        }
+
+        public static IModbusMaster CreateSerialMaster(this IModbusFactory factory, SerialSettings settings)
+        {
+            SerialPort serialPort = new SerialPort()
+            {
+                PortName = settings.PortName,
+                BaudRate = settings.BaudRate,
+                DataBits = settings.DataBits,
+                Parity = settings.Parity,
+                StopBits = settings.StopBits,
+                Handshake = settings.Handshake
+            };
+
+            var adapter = new SerialPortAdapter(serialPort);
+
+            serialPort.Open();
+
+            switch (settings.ModbusType)
+            {
+                case ModbusType.Rtu:
+                    return factory.CreateRtuMaster(adapter);
+                case ModbusType.Ascii:
+                    return factory.CreateAsciiMaster(adapter);
+                default:
+                    throw new ArgumentException("Serial Settings must be either of type Rtu or Ascii.");
+            }
+        }
+    }
+}
--- src/Service/ModbusMasterManager.cs
+++ src/Service/ModbusMasterManager.cs
@@ -1,12 +1,8 @@
 ﻿using NModbus.UI.Common.Core;
 using System;
 using System.Collections.Generic;
-using System.Net.Sockets;
-using NModbus.Serial;
-using System.IO.Ports;
 using Prism.Events;
 using System.Linq;
-using NModbus.IO;
 
 namespace NModbus.UI.Service
 {
@@ -33,121 +29,35 @@ namespace NModbus.UI.Service
 
         private void NewConnection(ConnectionSettings connectionSettings)
         {
-            if (connectionSettings is IpSettings)
-                NewIpConnection(connectionSettings as IpSettings);
-            else if (connectionSettings is SerialSettings)
-                NewSerialConnection(connectionSettings as SerialSettings);
-            else
-                throw new ArgumentException("connectionSettings");
-        }
-
-        private void NewIpConnection(IpSettings ipSettings)
-        {
             try
             {
-                string masterId = ipSettings.Hostname;
-                var factory = new ModbusFactory();
-
-                switch (ipSettings.ModbusType)
-                {
-                    case ModbusType.Tcp:
-                        CreateTcpMaster(ipSettings);
-                        break;
-                    case ModbusType.Udp:
-                        CreateTcpMaster(ipSettings);
-                        break;
-                    case ModbusType.RtuOverTcp:
-                        CreateRtuOverTcpMaster(ipSettings);
-                        break;
-                    case ModbusType.RtuOverUdp:
-                        CreateRtuOverUdpMaster(ipSettings);
-                        break;
-                    default:
-                        throw new ArgumentException("Ip settings must be either of type Tcp or Udp.");
-                }
-
-                _ea.GetEvent<NewModbusMasterEvent>().Publish(masterId);
+                if (connectionSettings is IpSettings)
+                    CreateIpModbusMaster(connectionSettings as IpSettings);
+                else if (connectionSettings is SerialSettings)
+                    CreateSerialMaster(connectionSettings as SerialSettings);
+                else
+                    throw new ArgumentException("connectionSettings");
             }
             catch (Exception e)
             {
-                _ea.GetEvent<ExceptionEvent>().Publish(e);
+                PublishException(e);
             }
         }
 
-        private void CreateTcpMaster(IpSettings ipSettings)
+        private void CreateIpModbusMaster(IpSettings ipSettings)
         {
-            var tcpClient = new TcpClient();
-            tcpClient.Connect(ipSettings.Hostname, ipSettings.Port);
-            var tcpMaster = _modbusFactory.CreateMaster(tcpClient);
-            AddMaster(ipSettings.Hostname, tcpMaster);
-            
+            var master = _modbusFactory.CreateIpMaster(ipSettings);
+            string masterId = ipSettings.Hostname;
+            AddMaster(masterId, master);
+            _ea.GetEvent<NewModbusMasterEvent>().Publish(masterId);
         }
 
-        private void CreateUdpMaster(IpSettings ipSettings)
+        private void CreateSerialMaster(SerialSettings settings)
         {
-            var udpClient = new UdpClient();
-            udpClient.Connect(ipSettings.Hostname, ipSettings.Port);
-            var udpMaster = _modbusFactory.CreateMaster(udpClient);
-            AddMaster(ipSettings.Hostname, udpMaster);
-        }
-
-        private void CreateRtuOverTcpMaster(IpSettings ipSettings)
-        {
-            var tcpClient = new TcpClient();
-            tcpClient.Connect(ipSettings.Hostname, ipSettings.Port);
-            var adapter = new TcpClientAdapter(tcpClient);
-            var tcpMaster = _modbusFactory.CreateRtuMaster(adapter);
-            AddMaster(ipSettings.Hostname, tcpMaster);
-        }
-
-        private void CreateRtuOverUdpMaster(IpSettings ipSettings)
-        {
-            var udpClient = new UdpClient();
-            udpClient.Connect(ipSettings.Hostname, ipSettings.Port);
-            var adapter = new UdpClientAdapter(udpClient);
-            var udpMaster = _modbusFactory.CreateRtuMaster(adapter);
-            AddMaster(ipSettings.Hostname, udpMaster);
-        }
-
-        private void NewSerialConnection(SerialSettings settings)
-        {
-            try
-            {
-                string masterId = settings.PortName;
-                var factory = new ModbusFactory();
-                SerialPort serialPort = new SerialPort()
-                {
-                    PortName = settings.PortName,
-                    BaudRate = settings.BaudRate,
-                    DataBits = settings.DataBits,
-                    Parity = settings.Parity,
-                    StopBits = settings.StopBits,
-                    Handshake = settings.Handshake
-                };
-
-                var adapter = new SerialPortAdapter(serialPort);
-                serialPort.Open();
-
-                switch (settings.ModbusType)
-                {
-                    case ModbusType.Rtu:
-                        var rtuMaster = factory.CreateRtuMaster(adapter);
-                        AddMaster(masterId, rtuMaster);
-                        break;
-                    case ModbusType.Ascii:
-                        var asciiMaster = factory.CreateAsciiMaster(adapter);
-                        AddMaster(masterId, asciiMaster);
-                        break;
-                    default:
-                        throw new ArgumentException("Serial Settings must be either of type Rtu or Ascii.");
-                }
-
-                _ea.GetEvent<NewModbusMasterEvent>().Publish(masterId);
-            }
-            catch (Exception e)
-            {
-                _ea.GetEvent<ExceptionEvent>().Publish(e);
-            }
+            var master = _modbusFactory.CreateSerialMaster(settings);
+            string masterId = settings.PortName;
+            AddMaster(masterId, master);
+            _ea.GetEvent<NewModbusMasterEvent>().Publish(masterId);
         }
 
 #if DEBUG
@@ -182,22 +92,29 @@ namespace NModbus.UI.Service
 
         private void ReadObjects(ModbusReadRequest request)
         {
-            switch (request.ObjectType)
+            try
             {
-                case ObjectType.Coil:
-                    ReadCoils(request);
-                    break;
-                case ObjectType.DiscreteInput:
-                    ReadDiscreteInputs(request);
-                    break;
-                case ObjectType.InputRegister:
-                    ReadInputRegisters(request);
-                    break;
-                case ObjectType.HoldingRegister:
-                    ReadHoldingRegisters(request);
-                    break;
-                default:
-                    throw new ArgumentException("request");
+                switch (request.ObjectType)
+                {
+                    case ObjectType.Coil:
+                        ReadCoils(request);
+                        break;
+                    case ObjectType.DiscreteInput:
+                        ReadDiscreteInputs(request);
+                        break;
+                    case ObjectType.InputRegister:
+                        ReadInputRegisters(request);
+                        break;
+                    case ObjectType.HoldingRegister:
+                        ReadHoldingRegisters(request);
+                        break;
+                    default:
+                        throw new ArgumentException("request");
+                }
+            }
+            catch (Exception e)
+            {
+                PublishException(e);
             }
         }
 
@@ -205,28 +122,33 @@ namespace NModbus.UI.Service
         {
             bool[] result = _masters[request.MasterId].ReadInputs(
                 request.SlaveId, request.StartAddress, request.NumberOfPoints);
-            _ea.GetEvent<ModbusReadResponseEvent>().Publish(CreateResponse(request, result));
+            PublishResponse(CreateResponse(request, result));
         }
 
         private void ReadCoils(ModbusReadRequest request)
         {
             bool[] result = _masters[request.MasterId].ReadCoils(
                 request.SlaveId, request.StartAddress, request.NumberOfPoints);
-            _ea.GetEvent<ModbusReadResponseEvent>().Publish(CreateResponse(request, result));
+            PublishResponse(CreateResponse(request, result));
         }
 
         private void ReadInputRegisters(ModbusReadRequest request)
         {
             UInt16[] result = _masters[request.MasterId].ReadInputRegisters(
                 request.SlaveId, request.StartAddress, request.NumberOfPoints);
-            _ea.GetEvent<ModbusReadResponseEvent>().Publish(CreateResponse(request, result));
+            PublishResponse(CreateResponse(request, result));
         }
 
         private void ReadHoldingRegisters(ModbusReadRequest request)
         {
             UInt16[] result = _masters[request.MasterId].ReadHoldingRegisters(
                 request.SlaveId, request.StartAddress, request.NumberOfPoints);
-            _ea.GetEvent<ModbusReadResponseEvent>().Publish(CreateResponse(request, result));
+            PublishResponse(CreateResponse(request, result));
+        }
+
+        private void PublishResponse(ModbusReadResponse response)
+        {
+            _ea.GetEvent<ModbusReadResponseEvent>().Publish(response);
         }
 
         private ModbusReadResponse CreateResponse(ModbusReadRequest request, dynamic data)
@@ -239,6 +161,11 @@ namespace NModbus.UI.Service
                 StartAddress = request.StartAddress,
                 Data = data
             };
+        }
+
+        private void PublishException(Exception e)
+        {
+            _ea.GetEvent<ExceptionEvent>().Publish(e);
         }
 
         public void Dispose()
